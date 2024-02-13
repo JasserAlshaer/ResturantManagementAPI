ba8b70bcc195808beea259cce703702486938a37
Akram Injaileh <akraminjaileh@gmail.com>
2024-02-13 20:48:17 +0300

    Add Prop for Meal Class

--- ResturantManagementAPI/Models/Entites/Meal.cs
+++ ResturantManagementAPI/Models/Entites/Meal.cs
@@ -2,5 +2,12 @@
 {
     public class Meal
     {
+        public int MealID { get; set; }
+        public int ItemId { get; set; }
+        public int CartItemMeal { get; set; }
+        public string Name  { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string Image { get; set; }
     }
 }
