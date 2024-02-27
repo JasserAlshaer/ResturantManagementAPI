0073eecc5992561018194f5f6fbd9bf78136660c
ASUS <ASUS@DESKTOP-SV986PK>
2024-02-27 20:54:56 +0300

    commited

---
+++ ResturantManagementAPI/Dtos/Menu/MenuItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ResturantManagementAPI.Dtos.Menu
+{
+    public class MenuItemDTO
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Qtn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
--- ResturantManagementAPI/Interfaces/ISharedInterface.cs
+++ ResturantManagementAPI/Interfaces/ISharedInterface.cs
@@ -1,9 +1,16 @@
 ﻿using ResturantManagementAPI.Dtos.Login;
+using ResturantManagementAPI.Dtos.Menu;
 
 namespace ResturantManagementAPI.Interfaces
 {
     public interface ISharedInterface
     {
-        Task Login(LoginReqDTO dto); 
+        Task Login(LoginReqDTO dto);
+        //Logout
+        //CreateAccount
+        //ResetPassword
+        //UpdateProfile
+        //ViewMenu
+        Task<List<MenuItemDTO>> ViewMenu(bool isAdmin = false);
     }
 }
